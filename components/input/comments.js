6101a50a92ac40f915104f0ea1bad3b4b010bf01
Max Girteit <icufishmg@gmail.com>
2022-06-25 11:17:33 -0600

    comment functionality added

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,17 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
 
+
 function Comments(props) {
   const { eventId } = props;
 
   const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState([])
 
   function toggleCommentsHandler() {
     setShowComments((prevStatus) => !prevStatus);
+
+
   }
+
+useEffect(() => {
+if(showComments) {
+  fetch('/api/comments/' + eventId).then(response => response.json()).then((data) => {
+    setComments(data.comments);
+  })
+}
+}, [])
+
 
   function addCommentHandler(commentData) {
     fetch('/api/comments/' + eventId, {
@@ -31,7 +44,7 @@ function Comments(props) {
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList />}
+      {showComments && <CommentList items={comments}/>}
     </section>
   );
 }
