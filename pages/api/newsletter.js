860bef25ce040b1b218b0e17a672d3638f648953
Max Girteit <icufishmg@gmail.com>
2022-06-25 14:53:07 -0600

    moved to db-util

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,17 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-async function connectDatabase() {
-  const client = await MongoClient.connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.21ehf.mongodb.net/events?retryWrites=true&w=majority`
-  );
-
-  return client;
-}
-
-async function insertDocument(client, document) {
-  const db = client.db();
-  await db.collection('newsletter').insertOne(document);
-}
+import { connectDatabase, insertDocument } from '../../helpers/db-util';
 
 async function handler(req, res) {
   if (req.method === 'POST') {
@@ -32,10 +21,10 @@ async function handler(req, res) {
     }
 
     try {
-      await insertDocument(client, { email: userEmail });
+      await insertDocument(client, 'newsletter', { email: userEmail });
       client.close();
     } catch (err) {
-      res.status(500).json({message: 'Inserting data failed.'})
+      res.status(500).json({ message: 'Inserting data failed.' });
     }
 
     res.status(201).json({ message: 'Signed up!' });
