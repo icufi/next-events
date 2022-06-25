0b2f0885aa6e2c4c1482961cbb86d31c07e66cc3
Max Girteit <icufishmg@gmail.com>
2022-06-25 15:03:43 -0600

    getAllDocuments added to db-util

--- helpers/db-util.js
+++ helpers/db-util.js
@@ -11,3 +11,16 @@ export async function insertDocument(client, collection, document) {
   const result = await db.collection(collection).insertOne(document);
   return result;
 }
+
+
+export async function getAllDocuments(client, collection, sort) {
+const db = client.db();
+
+const documents = await db
+  .collection(collection)
+  .find()
+  .sort(sort)
+  .toArray();
+
+  return documents;
+}
