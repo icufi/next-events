122abbe4a03f003a9d04673065568ac0c7653e0a
Max Girteit <icufishmg@gmail.com>
2022-06-25 11:44:22 -0600

    add user email to db functionality

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -7,7 +9,14 @@ function handler(req, res) {
       return;
     }
 
-    console.log(userEmail);
+    const client = await MongoClient.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_Pass}@cluster0.21ehf.mongodb.net/?retryWrites=true&w=majority`
+    );
+    const db = client.db();
+    await db.collection('emails').insertOne({ email: userEmail });
+
+    client.close();
+
     res.status(201).json({ message: 'Signed up!' });
   }
 }
