c90a55088222a2cc143f9f4a96fa831a5eeda7a4
Max Girteit <icufishmg@gmail.com>
2022-06-26 12:24:20 -0600

    notification timer added in useEffect w/ cleanup

--- store/notifiation-context.js
+++ store/notifiation-context.js
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const NotificationContext = createContext({
   notification: null, //{title,message,status}
@@ -9,15 +9,35 @@ const NotificationContext = createContext({
 export function NotificationContextProvider(props) {
   const [activeNotification, setActiveNotification] = useState();
 
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === 'success' ||
+        activeNotification.status === 'error')
+    ) {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 3000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, []);
+
   function showNotificationHandler(notificationData) {
-    setActiveNotification({ notificationData  });
+    setActiveNotification({ notificationData });
   }
 
   function hideNotificationHandler() {
-    setActiveNotification(null)
+    setActiveNotification(null);
   }
 
-  const context = {notification: activeNotification, showNotification: showNotificationHandler, hideNotification: hideNotificationHandler}
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
 
   return (
     <NotificationContext.Provider value={context}>
