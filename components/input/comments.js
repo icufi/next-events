c91f0d43b039b97eb51b369ce91678fe8219b8c7
Max Girteit <icufishmg@gmail.com>
2022-06-27 10:01:24 -0600

    error handling added

--- components/input/comments.js
+++ components/input/comments.js
@@ -9,6 +9,8 @@ import NotificationContext from '../../store/notification-context';
 function Comments(props) {
   const { eventId } = props;
 
+  const notificationCtx = contuseContext(NotificationContext);
+
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([])
 
@@ -28,6 +30,11 @@ if(showComments) {
 
 
   function addCommentHandler(commentData) {
+     notificationCtx.showNotification({
+       title: 'Sending comment...',
+       message: 'Your comment is being stored in out database.',
+       status: 'pending',
+     });
     fetch('/api/comments/' + eventId, {
       method: 'POST',
       body: JSON.stringify(commentData),
@@ -35,8 +42,27 @@ if(showComments) {
         'Content-Type': 'application/json',
       },
     })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      if (response.ok) {
+          return response.json()
+        }
+
+        return response.json().then(data => {
+          throw new Error(data.message) || "Something went wrong!"
+        })
+      .then((data) => {
+         notificationCtx.showNotification({
+           title: 'Success!',
+           message: 'Your comment was saved.',
+           status: 'pending',
+         });
+      })
+      .catch(error => {
+        notificationCtx.showNotification({
+          title: 'Error!',
+          message: error.message || 'Something went wrong!',
+          status: 'success'
+        });
+      })
   }
 
   return (
